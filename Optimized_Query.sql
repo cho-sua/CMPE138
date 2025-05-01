8d3f1c971cefa395cc1d039056e492f5e929cd37
cho-sua <joshuarieta04@gmail.com>
2025-05-01 15:42:54 -0700

    Update Optimized_Query.sql
    Updated 2nd optimized query

--- Optimized_Query.sql
+++ Optimized_Query.sql
@@ -11,9 +11,10 @@ WHERE
   nppes_provider_state = 'FL'
 LIMIT 1000;
 
--- Optimized Query 2 (from Query 6) List all hostpitals 
-SELECT DISTINCT x.hospital_name, x.hospital_ownership, x.address, x.city, x.hospital_overall_rating, x.county_name,x.phone_number
+-- Optimized Query 2 (from Query 5) List all hostpitals, their general information, and current patient diagnoses
+SELECT DISTINCT x.hospital_name, x.hospital_ownership, x.address, x.city, x.hospital_overall_rating, x.county_name,x.phone_number, y.hospital_referral_region_description as region, y.drg_definition
 FROM `bigquery-public-data.cms_medicare.hospital_general_info` x
 JOIN `bigquery-public-data.cms_medicare.inpatient_charges_2014` y
 ON x.provider_id =y.provider_id
-LIMIT 20
+WHERE y.drg_definition IS NOT NULL
+LIMIT 100
